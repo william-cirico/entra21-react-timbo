bf5f1d0467dc5b079de6a3d3bc8e65003e124d5d
William <william.cirico@prof.sc.senac.br>
2021-08-06 11:38:41 -0300

    Correção exercício 03

--- Aula 46/fileSystem/escrevendoArquivos.js
+++ Aula 46/fileSystem/escrevendoArquivos.js
@@ -3,40 +3,41 @@ const fsPromises = require("fs/promises");
 const path = require("path");
 const { EOL } = require("os");
 
+console.log(EOL);
+
 // Escrevendo informações em um arquivo (Callback)
-const conteudo = `Esse texto foi criado através de um arquivo no node${EOL}Essa é uma nova linha`;
+// const conteudo = `Esse texto foi criado através de um arquivo no node${EOL}Essa é uma nova linha`;
 
-// Se o arquivo já existir ele será sobrescrevido
-fs.writeFile(path.resolve(__dirname, "teste2.txt"), conteudo, err => {
-    if (err) {
-        return console.log(err.message);
-    }
+// // Se o arquivo já existir ele será sobrescrevido
+// fs.writeFile(path.resolve(__dirname, "teste2.txt"), conteudo, err => {
+//     if (err) {
+//         return console.log(err.message);
+//     }
 
-    console.log("O arquivo teste2.txt foi criado com sucesso!");
-});
+//     console.log("O arquivo teste2.txt foi criado com sucesso!");
+// });
 
 // Escrevendo informações em um arquivo (Promise)
-(async () => {
-    const conteudo = `Esse texto foi criado através de um arquivo no node${EOL}Essa é uma nova linha`;
+// (async () => {
+//     const conteudo = `Esse texto foi criado através de um arquivo no node${EOL}Essa é uma nova linha`;
 
-    try {
-        fsPromises.writeFile(path.resolve(__dirname, "teste2.txt"), conteudo);
-        console.log("O arquivo teste2.txt foi criado com sucesso!");
-    } catch(err) {
-        console.log(err);
-    }
-})();
+//     try {
+//         await fsPromises.writeFile(path.resolve(__dirname, "teste2.txt"), conteudo);
+//         console.log("O arquivo teste2.txt foi criado com sucesso!");
+//     } catch(err) {
+//         console.log(err);
+//     }
+// })();
 
 // Adicionando informações em um arquivo (Callback)
-const novoConteudo = EOL + "Uma nova linha foi adicionada :D";
-fs.appendFile(path.resolve(__dirname, "teste2.txt"), novoConteudo, err => {
-    if (err) {
-        return console.log(err.message);
-    }
+// const novoConteudo = EOL + "Uma nova linha foi adicionada :D";
+// fs.appendFile(path.resolve(__dirname, "teste2.txt"), novoConteudo, err => {
+//     if (err) {
+//         return console.log(err.message);
+//     }
 
-    console.log("Novo conteúdo adicionado (Callback)");
-});
-
+//     console.log("Novo conteúdo adicionado (Callback)");
+// });
 
 // Adicionando informações em um arquivo (Promise)
 (async () => {
